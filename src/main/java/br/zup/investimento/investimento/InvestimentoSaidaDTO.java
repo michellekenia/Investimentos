e4e94d973593456ed17b1de130ed2990542e224b
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-23 18:23:04 -0300

    Criar dto de saida

--- src/main/java/br/zup/investimento/investimento/InvestimentoSaidaDTO.java
+++ src/main/java/br/zup/investimento/investimento/InvestimentoSaidaDTO.java
@@ -1,6 +1,42 @@
 package br.zup.investimento.investimento;
 
-public class SaidaDTO {
+public class InvestimentoSaidaDTO {
 
-    private double valorInvestido; 
+    private double valorInvestido;
+    private double valorTotalDoLucro;
+    private double valorTotal;
+
+    public InvestimentoSaidaDTO() {
+    }
+
+    public InvestimentoSaidaDTO(double valorInvestido, double valorTotalDoLucro, double valorTotal) {
+        this.valorInvestido = valorInvestido;
+        this.valorTotalDoLucro = valorTotalDoLucro;
+        this.valorTotal = valorTotal;
+    }
+
+    public double getValorInvestido() {
+        return valorInvestido;
+    }
+
+    public void setValorInvestido(double valorInvestido) {
+        this.valorInvestido = valorInvestido;
+    }
+
+    public double getValorTotalDoLucro() {
+        return valorTotalDoLucro;
+    }
+
+    public void setValorTotalDoLucro(double valorTotalDoLucro) {
+        this.valorTotalDoLucro = valorTotalDoLucro;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
 }
