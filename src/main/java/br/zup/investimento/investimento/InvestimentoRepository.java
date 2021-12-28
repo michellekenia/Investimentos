29e595bf928ddbf7c0e9eca03738ddc1b3dd2e37
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-28 16:15:21 -0300

    Criar filtro para tipo de risco

--- src/main/java/br/zup/investimento/investimento/InvestimentoRepository.java
+++ src/main/java/br/zup/investimento/investimento/InvestimentoRepository.java
@@ -1,7 +1,11 @@
 package br.zup.investimento.investimento;
 
+import br.zup.investimento.investimento.enuns.Risco;
 import br.zup.investimento.investimento.model.Investimento;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface InvestimentoRepository extends CrudRepository <Investimento, Integer> {
+    List<Investimento> findAllByRisco (Risco risco);
 }
