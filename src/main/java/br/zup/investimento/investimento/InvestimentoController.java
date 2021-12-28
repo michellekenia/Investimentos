27344a286ed4573982047f52871a8d7872c7db3d
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-28 18:30:26 -0300

    Criar metodo que retorna lista dos investimentos

--- src/main/java/br/zup/investimento/investimento/InvestimentoController.java
+++ src/main/java/br/zup/investimento/investimento/InvestimentoController.java
@@ -2,13 +2,16 @@ package br.zup.investimento.investimento;
 
 import br.zup.investimento.investimento.dtos.InvestimentoDTO;
 import br.zup.investimento.investimento.dtos.InvestimentoSaidaDTO;
+import br.zup.investimento.investimento.dtos.InvestimentoResumoDTO;
+import br.zup.investimento.investimento.enuns.Risco;
+import br.zup.investimento.investimento.model.Investimento;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/investimentos")
@@ -16,14 +19,25 @@ public class InvestimentoController {
     @Autowired
     private InvestimentoService investimentoService;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @PutMapping
     public InvestimentoSaidaDTO realizarInvestimento (@RequestBody @Valid InvestimentoDTO investimentoDTO) {
         InvestimentoSaidaDTO investimentoSaidaDTO = investimentoService.salvarInvestimento(investimentoDTO);
 
         return investimentoSaidaDTO;
     }
+    @GetMapping
+    public List <InvestimentoResumoDTO> exibirTodosOsInvestimentos (@RequestParam (required = false) Risco risco){
 
+        List<InvestimentoResumoDTO> investimentosDTOS = new ArrayList<>();
+        for (Investimento investimento: investimentoService.exibirTodosOsinvestimentos(risco)){
+            InvestimentoResumoDTO resumoDTO = modelMapper.map(investimento, InvestimentoResumoDTO.class);
+            investimentosDTOS.add(resumoDTO);
+        }
 
-
+        return investimentosDTOS;
+    }
 
 }
