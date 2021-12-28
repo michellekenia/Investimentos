866628d339fa94004a7e0071b4cb93cd965532a0
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-28 16:47:32 -0300

    Criar excecao para enum invalido

--- src/main/java/br/zup/investimento/investimento/config/ControllerAdvisor.java
+++ src/main/java/br/zup/investimento/investimento/config/ControllerAdvisor.java
@@ -3,6 +3,7 @@ package br.zup.investimento.investimento.config;
 import br.zup.investimento.investimento.exceptions.periodoInferiorAoPermitidoException;
 import br.zup.investimento.investimento.exceptions.valorNaoPermitidoParaRiscoAltoException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,10 +18,10 @@ public class ControllerAdvisor {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public List<ErroDeValidacao> manipularErrosDeValidacao(MethodArgumentNotValidException exception){
+    public List<ErroDeValidacao> manipularErrosDeValidacao(MethodArgumentNotValidException exception) {
         List<ErroDeValidacao> erros = new ArrayList<>();
 
-        for (FieldError fieldError : exception.getFieldErrors()){
+        for (FieldError fieldError : exception.getFieldErrors()) {
             ErroDeValidacao erroDeValidacao = new ErroDeValidacao(fieldError.getField(),
                     fieldError.getDefaultMessage());
             erros.add(erroDeValidacao);
@@ -31,14 +32,23 @@ public class ControllerAdvisor {
 
     @ExceptionHandler(valorNaoPermitidoParaRiscoAltoException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public MensagemDeErro manipularExcecaoDeValorNaoPermitidoParaRiscoAlto(valorNaoPermitidoParaRiscoAltoException exception){
+    public MensagemDeErro manipularExcecaoDeValorNaoPermitidoParaRiscoAlto(valorNaoPermitidoParaRiscoAltoException exception) {
         return new MensagemDeErro(exception.getMessage());
     }
 
     @ExceptionHandler(periodoInferiorAoPermitidoException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public MensagemDeErro manipularExcecaoDePeriodoInferiorAoPermitido(periodoInferiorAoPermitidoException exception){
+    public MensagemDeErro manipularExcecaoDePeriodoInferiorAoPermitido(periodoInferiorAoPermitidoException exception) {
         return new MensagemDeErro(exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MensagemDeErro manipularExcecaoDeEnumInvalido(HttpMessageNotReadableException exception) {
+        if (exception.getLocalizedMessage().contains("br.zup.investimento.investimento.enuns.Risco")) {
+            return new MensagemDeErro("Esse tipo de risco não é válido.");
+        }
+        return new MensagemDeErro(exception.getLocalizedMessage());
     }
 
 }
