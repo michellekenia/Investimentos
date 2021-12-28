25265b4cf7d9d1d5f75dbddff4bea034f8ad13e8
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-28 16:01:04 -0300

    Criar metodo que realiza investimento na controller

--- src/main/java/br/zup/investimento/investimento/InvestimentoController.java
+++ src/main/java/br/zup/investimento/investimento/InvestimentoController.java
@@ -1,4 +1,29 @@
 package br.zup.investimento.investimento;
 
+import br.zup.investimento.investimento.dtos.InvestimentoDTO;
+import br.zup.investimento.investimento.dtos.InvestimentoSaidaDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/investimentos")
 public class InvestimentoController {
+    @Autowired
+    private InvestimentoService investimentoService;
+
+    @PutMapping
+    public InvestimentoSaidaDTO realizarInvestimento (@RequestBody @Valid InvestimentoDTO investimentoDTO) {
+        InvestimentoSaidaDTO investimentoSaidaDTO = investimentoService.salvarInvestimento(investimentoDTO);
+
+        return investimentoSaidaDTO;
+    }
+
+
+
+
 }
